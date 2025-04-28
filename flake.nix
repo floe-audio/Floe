cb415f336f91bbefc6488e73dcab053bc55012f0
Sam Windell <info@frozenplain.com>
2025-04-28 08:52:37 +0100

    nix: fix wrong url for unstable channel

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
