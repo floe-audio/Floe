170af7471ba0353d90c1280a722d4b05b5c2f1f2
Sam Windell <info@frozenplain.com>
2025-10-14 08:22:03 +0100

    fix spellings and missing lisenses

--- src/common_infrastructure/preset_pack_info.cpp
+++ src/common_infrastructure/preset_pack_info.cpp
@@ -1,3 +1,6 @@
+// Copyright 2025 Sam Windell
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "preset_pack_info.hpp"
 
 PresetPackInfo ParsePresetPackInfoFile(String file_data, ArenaAllocator& arena) {
